aad538610b9e7c5e6faa3b6db87716c8fa480a42
Haruna-ADUSTECH <harunaali@kustwudil.edu.ng>
2024-08-27 02:51:56 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleDetails(id) {
+    const details = document.getElementById('details-' + id);
+    if (details.style.display === 'none' || details.style.display === '') {
+        details.style.display = 'block';
+    } else {
+        details.style.display = 'none';
+    }
+}
